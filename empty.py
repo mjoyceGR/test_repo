9f6b2828e922a8a7fd38f9b04dba82637e0a126b
M. Joyce <meridith.joyce@gmail.com>
2025-02-06 17:55:38 -0700

    Create empty.py to test history conflicts

---
+++ empty.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.8
+####################################################
+#
+# Author: M Joyce
+#
+####################################################
+
+import numpy as np
+
+### this is an empty .py file so I can test against history conflicts
